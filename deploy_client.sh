0e34c4de99a73028d99bf8df253211641b911ed5
Liangyu Zhao <liangyu@cs.washington.edu>
2021-12-02 02:16:02 -0800

    fix client deployment

--- deploy_client.sh
+++ deploy_client.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-client_ip_configs="client_ips.config"
 server_ip_configs="server_ips.config"
 client_artifact="out/artifacts/paxos_client_jar/paxos_client.jar"
 
-while IFS= read -r line
-do
-  scp "${client_artifact}" "user@${line}:~/paxos_client.jar"
-  scp "${server_ip_configs}" "user@${line}:~/${server_ip_configs}"
-done < "${client_ip_configs}"
+location=$1
 
+scp "${client_artifact}" "${location}:~/paxos_client.jar"
+scp "${server_ip_configs}" "${location}:~/${server_ip_configs}"
 
-while IFS= read -r line
-do
-  ssh "user@${line}" "java -jar ~/paxos_client.jar ~/${server_ip_configs}"
-done < "${client_ip_configs}"
+ssh "${location}" "java -jar ~/paxos_client.jar ~/${server_ip_configs}"
