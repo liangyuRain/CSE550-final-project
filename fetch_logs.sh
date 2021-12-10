50ef7f6296e4e3009da3c828caa6828c9bdd7472
Liangyu Zhao <liangyu@cs.washington.edu>
2021-12-10 01:07:33 -0800

    fetch_logs.sh fetch *.hprof

--- fetch_logs.sh
+++ fetch_logs.sh
@@ -4,7 +4,23 @@ client_ip_configs="client_ips.config"
 server_ip_configs="server_ips.config"
 
 mkdir "log"
-rm -f ./log/*.log
+rm -f ./log/*.log ./log/*.hprof
+
+IPs=""
+while IFS= read -r line
+do
+  IPs="${IPs}${line%:*},"
+done < "${client_ip_configs}"
+while IFS= read -r line
+do
+  IPs="${IPs}${line%:*},"
+done < "${server_ip_configs}"
+IPs=$(echo "${IPs%,*}" | tr ',' '\n' | sort -u)
+
+while IFS= read -r line
+do
+  scp "user@${line}:~/*.hprof" "./log/"
+done <<< "${IPs}"
 
 while IFS= read -r line
 do
